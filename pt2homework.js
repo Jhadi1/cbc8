235969640db52355d9ed148a8cd6af47cb67953d
jhadi1 <jayhad662@gmail.com>
2020-01-30 15:53:18 -0500

    first commit of HW 01.30

--- pt2homework.js
+++ pt2homework.js
@@ -3,3 +3,53 @@ var second = " and this is Javascript.. Yayy! lol"
 console.log(first + second);
 var age = 39;
 console.log(first * age);
+
+while (age < 39){
+console.log("Your age is less than 45");
+age++;
+}
+
+console.log("you are now less than 45");
+var countThis = 2;
+console.log(countThis + 5);
+console.log(countThis * 2);
+var countThisReturned = 7;
+console.log(countThis * countThisReturned);
+var multiplyThisReturned = 14;
+// I think the modulus of 14 and 3 will return the number 2 //
+
+console.log(multiplyThisReturned % 3);
+
+// I think that modulus 2 by 2 will return the number 0 //
+
+console.log(countThis % 2);
+
+// I was correct //
+
+if (countThis % 2 == 0){
+    console.log("I am happy");
+    
+} 
+else{
+    console.log("my age modulus 2 is not an even number");
+}
+if (countThis !==2 || multiplyThisReturned < 10) {
+    console.log("its ok");
+}else 
+console.log("Mi ah just a likkle lost but mi a wul it");
+
+if (multiplyThisReturned % countThis == 0 && countThisReturned > 15) {
+    console.log("this aint odd")
+} 
+else {
+console.log("one of these numbers might be odd or greater than 15")
+}
+
+
+
+
+
+
+
+
+
