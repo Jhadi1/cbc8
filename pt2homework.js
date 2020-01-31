01a505bff74253ee3b7cd0746cfac2d2fb83a2aa
jhadi1 <jayhad662@gmail.com>
2020-01-30 19:19:06 -0500

    2nd commit HW 01.30

--- pt2homework.js
+++ pt2homework.js
@@ -3,12 +3,10 @@ var second = " and this is Javascript.. Yayy! lol"
 console.log(first + second);
 var age = 39;
 console.log(first * age);
-
 while (age < 39){
 console.log("Your age is less than 45");
 age++;
 }
-
 console.log("you are now less than 45");
 var countThis = 2;
 console.log(countThis + 5);
@@ -16,19 +14,19 @@ console.log(countThis * 2);
 var countThisReturned = 7;
 console.log(countThis * countThisReturned);
 var multiplyThisReturned = 14;
-// I think the modulus of 14 and 3 will return the number 2 //
+
+// I think the modulus of multiplyThisReturned and 3 will return the number 2
 
 console.log(multiplyThisReturned % 3);
 
-// I think that modulus 2 by 2 will return the number 0 //
+// I think that the modulus of 2 and 2 will return the number 0 
 
 console.log(countThis % 2);
 
-// I was correct //
+// I was correct 
 
 if (countThis % 2 == 0){
     console.log("I am happy");
-    
 } 
 else{
     console.log("my age modulus 2 is not an even number");
@@ -38,18 +36,26 @@ if (countThis !==2 || multiplyThisReturned < 10) {
 }else 
 console.log("Mi ah just a likkle lost but mi a wul it");
 
-if (multiplyThisReturned % countThis == 0 && countThisReturned > 15) {
-    console.log("this aint odd")
+if (multiplyThisReturned % countThis == 0 && countThisReturned > 15){
+    console.log("this aint odd");
 } 
 else {
-console.log("one of these numbers might be odd or greater than 15")
+console.log("one of these numbers might be odd or greater than 15");
+}
+if (countThisReturned > 5){
+    console.log("OKGreat");
+}
+if (countThis < 4){
+console.log("apples");
+}
+if(multiplyThisReturned % 2 == 0){
+    console.log("bananas");
+}
+if (countThis > 7 && 9){
+    console.log("pears");
 }
 
-
-
-
-
-
+// multiple if lines seem to add unecessary code almost like a run-on sentence.
 
 
 
