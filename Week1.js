1ceaa18c270ad141518bec09541d577a2835dfc1
jhadi1 <jayhad662@gmail.com>
2020-02-17 13:50:24 -0500

    testing

--- Week1.js
+++ Week1.js
@@ -121,16 +121,40 @@ t++;
         }
     }
 }
-    
 //Exercise Math
+var staples = ["chicken", "cheese", "milk", "eggs", "fish", "cereal", "apples", "onions", "bread", "potatoes"];
 function randomCheck(){
-    var computerGen = Math.random();
-    var userGuess = document.getElementById("userInput").value;
-  if (userGuess == computerGen) {
-  console.log("we have a match!");
-  }
-  else {
-      console.log("no match");
+    var text = document.getElementById("input").value;{
+  if (input==staples[0]) {
+    console.log("staples=-1");
   }
 }
+}
 
+//Exercise Array
+//Question1
+var staples = ["chicken", "cheese", "milk", "eggs", "fish", "cereal", "apples", "onions", "bread", "potatoes"];
+staples.forEach(function(value, index){
+console.log(value, index);
+});
+//Question2
+var list = "Jamilla";
+var wordLength=list.length;
+for (i=0; i< wordLength; i++){
+console.log(list[i]);
+}
+//Question3
+console.log(list.toUpperCase());
+//Exercise Array2
+var inventory = [["brush", 3], ["comb", 4], ["hair color", 5], ["hair spray", 2]];
+var hour = 19;
+if (hour>= 6 && hour < 12){
+console.log("Good morning");
+}
+else if (hour > 12 && hour<18) {
+    console.log("Good afternoon");
+    
+
+}
+else
+    console.log("Good evening");
