3d07edd735e2db5d33c2fcbb3a01e448a8d5488d
jhadi1 <jayhad662@gmail.com>
2020-02-18 09:20:25 -0500

    real time commit

--- Week2.js
+++ Week2.js
@@ -1,1 +1,7 @@
-
+let array = [brush, comb, mirror];
+const removeElement = function(a, index) {
+   let newArray = [...a];
+   newArray.splice(index, 1);
+    var str = document.getElementById("text1").value;
+return newArray;
+};
