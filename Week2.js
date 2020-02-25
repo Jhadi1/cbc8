c20058d86a2f31a2fae227d96816e01260fa1c50
jhadi1 <jayhad662@gmail.com>
2020-02-25 11:58:00 -0500

    HW week 4

--- Week2.js
+++ Week2.js
@@ -1,9 +1,17 @@
-let array = [brush, comb, mirror];
-const removeElement = function(a, index) {
-   let newArray = [...a];
-   newArray.splice(index, 1);
-    var str = document.getElementById("text1").value;
-return newArray;
-};
-
 //Exercise countdown
+function subone() {
+    var foo = document.getElementById("subone").innerHTML;
+    foo--;
+    document.getElementById("subone").innerHTML = foo;
+    }
+    let arry = ["brush", "comb", "mirror"];
+    <button onclick="remOne();" type="button" class="btn">Remove Item</button>
+    
+    let thingToPrint = "";
+    undefined
+    function drawText() {
+        thingToPrint += "#";
+        document.writeIn(thingToPrint);
+    }
+    undefined
+    setInterval (drawText, 2000);
